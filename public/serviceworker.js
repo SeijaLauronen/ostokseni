e048bdf85c8766ce52bf3f509f644c2a7568ad13
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-07-20 10:44:42 +0300

    Show products with unknown category on top.

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,6 @@
-const programVersion = '2024-07-20: 1.167';
-const staticCacheName = 'myshopping-static-167'; 
-const dynamicCacheName = 'myshopping-dynamic-167'; // Ei välttämätön
+const programVersion = '2024-07-20: 1.168';
+const staticCacheName = 'myshopping-static-168'; 
+const dynamicCacheName = 'myshopping-dynamic-168'; // Ei välttämätön
 
 const assets = [
   '/',
