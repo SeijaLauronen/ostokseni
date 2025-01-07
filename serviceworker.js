342bd9d9d152464141dc14591f510f0d4118064e
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2025-01-07 21:03:03 +0200

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
-const programVersion = '2025-01-05: 2.310';
-const staticCacheName = 'myshopping-static-2310'; 
-const dynamicCacheName = 'myshopping-dynamic-2310'; // Ei välttämätön
+const programVersion = '2025-01-07: 2.311';
+const staticCacheName = 'myshopping-static-2311'; 
+const dynamicCacheName = 'myshopping-dynamic-2311'; // Ei välttämätön
 
 const assets = [
   '/',
